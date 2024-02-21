6e3d9d088c8763b0e279a8443013d69e2759a0bd
Briden Dafoe <dafoebriden@gmail.com>
2024-02-21 15:29:29 -0700

    base

--- client/app/env.js
+++ client/app/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = ''
-export const audience = ''
-export const clientId = ''
+export const domain = 'dev-5cjrhfh4lcsm7qmz.us.auth0.com'
+export const audience = 'https://bridenscodeworksapi.com'
+export const clientId = '4jVGlcNjeInS4vAOTFxnNrS5bTLzN0vm'
